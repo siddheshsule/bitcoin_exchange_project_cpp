6ae6a6775a552c51fe4a0459eaafb44c9554788a
siddhesh.sule47@gmail.com <siddhesh.sule47@gmail.com>
2024-01-16 22:47:13 +0100

    Project functionality added

--- bitcoin.cpp
+++ bitcoin.cpp
@@ -10,6 +10,16 @@
 
 typedef std::unique_ptr<CURL, std::function<void(CURL*)>> CURL_ptr;
 
+
+extern "C" std::size_t dataHandler(const char* buffer, std::size_t size,
+    std::size_t nmemb, std::string userDate) {
+        if (userDate == nullptr) {
+            return 0;
+        }
+        userData->append(buffer, (size* nmemb));
+        return size* nmemb;
+    }
+
 class CurlHandle {
     private:
         CURL_ptr curlptr;
@@ -17,10 +27,50 @@ class CurlHandle {
             curl_easy_cleanup(c);
             curl_global_cleanup();
         };
+    public:
+        CurlHandle() : curlptr(curl_easy_init(), deleter) {
+            curl_global_init(CURL_GLOBAL_ALL);
+            curl_easy_setopt(curlptr.get(), CURLOPT_WRITERFUNCTION, dataHandler);
+            curl_easy_setopt(curlptr.get(), CURL_WRITEDATA, &data);
+            
+        }
+
+        void setUrl(std::string url) {
+            curl_easy_setopt(curlptr.get(), CURLOPT_URL, url.c_str());
+        }
+
+        CURLcode fetch() {
+            return curl_easy_perform(curlptr.get());
+        }
+
+        std::string getFetchedData() {
+            return data;
+        }
+};
+
+class Bitcoin {
+    using json = nlohmann::json;
+
+    private:
+        CurlHandle curlHandle;
+        static constexpr const char* API_URL = "https://blockchain.info/ticker";
+
+    public:
+        Bitcoin() : curlHandle({}) {
+            curlHandle.setUrl(API_URL);
+        }
+
+        json fetchBitcoinData() {
+            curlHandle.fetch();
+            return json::parse(curlHandle.getFetehedData());
+        }
 };
 
 
 int main() {
+
+    Bitcoin bitcoin;
+    bitcoin.fetchBitcoinData();
 
     std::cout << "Hello, world!" << std::endl;
 
